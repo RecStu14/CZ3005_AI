fc4754bec64ac2461fc7b01f57b90e66873d1bb0
Sherelyn Yap <yap.xuan.ying2001@gmail.com>
2022-02-23 13:16:17 +0800

    solve problem in Task2

--- Task2.py
+++ Task2.py
@@ -1,57 +1,61 @@
 import json, timeit
 from collections import deque # deque = Doubly Ended Queue
 
-def DFS(g, Dist, Cost, v, w):
+# g - python dict, v - start node, w - end node
+def BFS(g, Dist, Cost, v, w):
     # Queue to record adjacent nodes
     Pqueue = deque()
-    count = 0
-
-    # initialize list of (route, distCost, eCost) tuples from source vertex
+    
+    seen = []
+    # initialize list of (distCost, eCost, route) tuples from source vertex
     d = []
-    d = [ [ [], None, None] for i in range(len(g))] # infinity dist from source vertex = no path to target vertex
+    d = [ [None, None, []] for i in range(len(g))] # infinity dist from source vertex = no path to target vertex
 
-    d[int(v)-1][0].append(v)
-    d[int(v)-1][1] = 0
-    d[int(v)-1][2] = 0
+    d[int(v)-1][0] = 0 #indicate source vertex
+    d[int(v)-1][1] = 0 #indicate source vertex
+    d[int(v)-1][2].append(v)
+    seen.append(v)
     ptr = v
 
-    t1 = timeit.default_timer()
-    
     # while target vortex is not reached
-    while not d[int(w)-1][0]:
+    while True:
         for adj in g[ptr]:
-            tmp = d[int(ptr)-1][0][:]
-    
-            # only expand if unvisited
-            if tmp.count(adj) < 1:
-                Pqueue.appendleft(adj)
-                dCost = d[int(ptr)-1][1] + Dist[ptr+","+adj]
-                eCost = d[int(ptr)-1][2] + Cost[ptr+","+adj]
-                
-                if eCost <= 287932: 
-                    tmp.append(adj)
-                    
-                    # if not visited before, add prev distCost and edgeCost
-                    if d[int(adj)-1][1] is None:
-                        d[int(adj)-1][0] = tmp[:]
-                        d[int(adj)-1][1] = dCost
-                        d[int(adj)-1][2] = eCost
-                        
-                    # if visited but a better path found, replace
-                    elif dCost < d[int(adj)-1][1] and eCost < d[int(adj)-1][2]:
-                        d[int(adj)-1][0] = tmp[:]
-                        d[int(adj)-1][1] = dCost
-                        d[int(adj)-1][2] = eCost
-                        
+            eCost = d[int(ptr)-1][1] + Cost[ptr+","+adj]
             
-        t2 = timeit.default_timer()
+            # if unvisited and within budget
+            if seen.count(adj) < 1:
+                if eCost <= 287932:
+                    seen.append(adj) # mark visited
+                    Pqueue.append(adj)
 
-        #if no more nodes to expand or (t2-t1) > 15, break and return
-        if len(Pqueue) == 0 or (t2-t1) >= 15:
-            break
+                    dCost = d[int(ptr)-1][0] + Dist[ptr+","+adj]
+                    tmp = d[int(ptr)-1][2][:]
+                    tmp.append(adj) 
+
+                    d[int(adj)-1][0] = dCost
+                    d[int(adj)-1][1] = eCost
+                    d[int(adj)-1][2] = tmp[:]
+
+            # if visited but a better path found, replace
+            elif eCost < d[int(adj)-1][1]:
+                Pqueue.appendleft(adj)
+
+                dCost = d[int(ptr)-1][0] + Dist[ptr+","+adj]
+                tmp = d[int(ptr)-1][2][:]
+                tmp.append(adj)
+
+                d[int(adj)-1][0] = dCost
+                d[int(adj)-1][1] = eCost
+                d[int(adj)-1][2] = tmp[:]
+
+        # if no more nodes connected to source vertex
+        if len(Pqueue) == 0:
+            break        
         ptr = Pqueue.popleft()
-        
-    return d[int(w)-1]   
+    print()
+    # A path to target vertex is found that fulfills all condition
+    return d[int(w)-1]
+   
         
 # Open JSON file
 f = open('G.json')
@@ -68,20 +72,20 @@ while again == "y":
     end = input("end node [1-264346]: ")
     
     t2 = timeit.default_timer()
-    path = DFS(G_dict, Dist, Cost, start, end)
+    path = BFS(G_dict, Dist, Cost, start, end)
     t3 = timeit.default_timer()
 
-    if not path[0]: #if empty list returned
-        print("No path connecting " + start + " to " + end)
-    else:
+    if path[0] is not None:
+        print("Path Length: " + str(len(path[2])))
         print("Shortest Path:", end = " ")
-        for node in path[0]:
+        for node in path[2]:
             print(node, end ="->")
         print()
-        print("Shortest Distance: " + str(path[1]))
-        print("Total Energy Cost: " + str(path[2]))
+        print("Shortest Distance: " + str(path[0]))
+        print("Total Energy Cost: " + str(path[1]))
         print("Time taken: " + str(round(t3-t2, 3)))
         
     again = input("Do you want to find path again? (y/n)")
+
 # Closing file
 f.close()
